d0ecacafc4bad2ea2a37de366ecc709daae1b27d
Efrain Nevarez <efrain.nevarezjr@gmail.com>
2020-10-25 12:32:00 -0500

    added button element and time element to JS

--- script.js
+++ script.js
@@ -1,19 +1,16 @@
-// var currDate = new Date();
-// var currTime = currDate.getHours() + ":" + currDate.getMinutes() + ":" + currDate.getSeconds();
+$(document).ready(function () {
+  $(".save-button").click(function () {
+    var userInput = $(this).siblings(".saveText").val();
+    var timeClock = $(this).parent().attr("id");
+    localStorage.setItem(timeClock, userInput);
+  });
 
+  function hourClock() {
+    var currentTime = moment().hours();
+    $(".time-block").each(function () {
+      var hourSave = parseInt($(this).attr("id").split("-")[1]);
+    });
+  }
 
-function setDate (){
-    var currDate = new Date();
-    var currTime = currDate.getHours() + ":" + currDate.getMinutes() + ":" + currDate.getSeconds();
-document.getElementById("currentDay").innerHTML = write(currTime);
-}
-setDate();
-
-//Ok we gotta make a papa div with an id of hour-9 and class of row and time-block
-
-//NEXT make a child div with a class of hour(you will need to look at bootstrap to see how to make it small)
-
-//ext write a textarea next to the div with the hour and after that a button!
-
-//In the textarea make a class of description 
-//Make all those in one line to add up to 12
+  hourClock();
+});
